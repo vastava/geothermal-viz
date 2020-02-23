c4b8496c60c2c632ebef9165e1f3b387aa8cd0e6
vastava <37812945+vastava@users.noreply.github.com>
2020-02-22 23:00:36 -0800

    U.S., intro

---
+++ js/data.js
@@ -0,0 +1,57 @@
+var photos1 = [{
+    lat: 24.000,
+    lng: 99.000,
+    thumbnail: "img/cluster/people1.jpg",
+    url: "img/cluster/people1.jpg",
+    caption: "Local people overlooking the xiaowan construction site; Photo by Bryan Tilt"
+}, {
+    lat: 22.856,
+    lng: 103.637,
+    thumbnail: "img/cluster/people2.jpg",
+    url: "img/cluster/people2.jpg",
+    caption: "A local farmer; Photo by Bryan Tilt"
+},
+    {
+        lat: 25.425,
+        lng: 102.000,
+        thumbnail: "img/cluster/people3.jpg",
+        url: "img/cluster/people3.jpg",
+        caption: "The farmer walked to his agricultural site; Photo by Bryan Tilt"
+    },
+];
+
+var photos2 = [{
+    lat: 25.00122820195785,
+    lng:  98.75149536132814,
+    thumbnail: "img/cluster/house1.jpg",
+    url: "img/cluster/house1.jpg",
+    caption: "House in village; Photo by Bryan Tilt"
+}, {
+    lat: 24.897,
+    lng: 99.68145751953126,
+    thumbnail: "img/cluster/house2.jpg",
+    url: "img/cluster/house2.jpg",
+    caption: "Compensated house for Xiaowan; Photo by Bryan Tilt"
+}, {
+    lat:  24.43714709713302,
+    lng: 100.59288024902345,
+    thumbnail: "img/cluster/house3.jpg",
+    url: "img/cluster/house3.jpg",
+    caption: "House in village; Photo by Bryan Tilt"
+}, ];
+
+var photos3 = [{
+    lat:  24.897,
+    lng: 99.68145751953126,
+    thumbnail: "img/cluster/view1.jpg",
+    url: "img/cluster/view1.jpg",
+    caption: "Mountainous landscape; Photo by Bryan Tilt"
+}, {
+    lat: 24.437,
+    lng: 100.592,
+    thumbnail: "img/cluster/village1.jpg",
+    url: "img/cluster/village1.jpg",
+    caption: "A village downstream of Manwan site; Photo by Bryan Tilt",
+}]
+
+
